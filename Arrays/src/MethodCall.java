1f7a91dd3033d11810f55fd099b9696cb128ab21
Varshith <sriramoju.varshith@outlook.com>
2025-05-14 22:18:13 +0530

    commit

--- Arrays/src/MethodCall.java
+++ Arrays/src/MethodCall.java
@@ -1,14 +1,30 @@
 public class MethodCall {
     public static void main(String[] args) {
+
+        //call by value
         int value=10;
         MethodCall m1=new MethodCall();
         m1.displayValue(value);
         System.out.println("value from main "+value);
+
+        //call by reference
+        String[] name={"ab","cd","ef,","zx"}; //this is used in ref of disRef()
+        System.out.println(name); //[Ljava.lang.String;@1d81eb93 same address
+        System.out.println(name[0]);
+        MethodCall m2= new MethodCall();
+        m2.disRef(name);      //[Ljava.lang.String;@1d81eb93 same address
+        System.out.println("new value is "+name[0]);
     }
+    //call by value
     void displayValue(int value){
         System.out.println("value is "+value);
         value=20;
         System.out.println("value is "+value);
+    }
 
+    //call by reference
+    void disRef(String[] name){ //here string name is used the ref of main method sting
+        System.out.println(name[0]);
+        name[0]="xy";
     }
 }
---
+++ Java Collections/src/Arraylist.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Arraylist {
+    public static void main(String[] args) {
+        ArrayList<String> listName=new ArrayList<>(); //syntax
+        listName.add("varsh");
+        listName.add("abc");
+        listName.add("asd");
+        listName.add("asee");
+        listName.add("qwww");
+        System.out.println(listName); //print all elements
+//       // System.out.println(listName[0]);  ERROR
+//        for(String temp: listName){
+//            System.out.println(temp);
+//        }
+        System.out.println("the first element is :"+listName.get(0)); //print separate
+        listName.remove(1); //remove by index
+        listName.remove("varsh"); //remove by value
+        System.out.println(listName);
+        System.out.println(listName.set(2,"xyz")); //replace value
+        System.out.println(listName);
+        System.out.println(listName.indexOf("xyz")); //tells index
+
+    }
+}
