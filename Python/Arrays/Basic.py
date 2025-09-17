03a69192b0d59bb9560858446c0b5712928d349d
Varshith <sriramoju.varshith@outlook.com>
2025-09-17 16:18:46 +0530

    commit

---
+++ Python/Arrays/Basic.py
@@ -0,0 +1,8 @@
+#storage or container of similar items placed
+#py array module and numpy module
+#Homo array contains same DT
+#Hetro array contains Diff DT elements
+import array as ar
+a1=ar.array('i',[1,2,3])
+a2=ar.array('e',['s','d'])
+print(a1.typecode)
